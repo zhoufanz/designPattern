7c8b7401d61e2bac72a2dc0342042ff8acdd540f
周凡 <zhoufan@sunlight.bz>
2017-02-18 18:26:27 +0800

    分支测试

---
+++ src/com/company/adapter/fggfddgfgfgf.java
@@ -0,0 +1,10 @@
+package com.company.adapter;
+
+/**
+ * Created by zf on 2017/2/18.
+ */
+public class fggfddgfgfgf {
+    public static void main(String[] args) {
+        System.out.println("我爱你");
+    }
+}
